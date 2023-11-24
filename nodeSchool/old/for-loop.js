374d48a94a41677537750e23eac5f65d4289d693
Jack Johnson <jackantonyjohnson@gmail.com>
2023-11-24 16:38:25 +0000

    nodeSchool

---
+++ nodeSchool/old/for-loop.js
@@ -0,0 +1,8 @@
+let total = 0
+let limit = 10
+for (let i = 0; i < 10; i++) {  
+    // log the numbers 0 through 9  
+    
+    total += i
+  }  
+  console.log(total)
---
+++ nodeSchool/old/rounding-numbers.js
@@ -0,0 +1,5 @@
+let roundup=1.5
+Math.round(1.5)
+
+let rounded = Math.round(1.5)
+console.log(rounded)
